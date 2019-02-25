e7590006328679f72c340e4393463390fb8e3446
tom-butler <tombutler.development@gmail.conm>
2019-02-25 12:24:26 +1100

    initial commit

---
+++ assets/update_ranges.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# Index new datasets and update ranges for WMS
+# should be run after archiving old datasets so that
+# ranges for WMS are correct
+# environment variables:
+# Usage: -p prefix(es) for search. If multiple use space seperated list enclosed in quotes
+#        -b bucket containing data
+#        -s suffix for search (optional). If multiple use space separated list enclosed in quotes
+#                                         If multiple must be same length as prefix list,
+#                                         if only one provided, suffix will be applied to ALL prefixes
+#        -y UNSAFE: If set script will use unsafe YAML reading. Only set if you fully trust source
+#        -d product to update in database (optional)
+# e.g. ./update_ranges -b dea-public-data -p "L2/sentinel-2-nrt/S2MSIARD/2018 L2/sentinel-2-nrt/2017"
+
+usage() { echo "Usage: $0 -u <protocol> -p <prefix> -b <bucket> [-s <suffix>] [-y UNSAFE]" 1>&2; exit 1; }
+
+while getopts ":u:p:b:s:y:d:" o; do
+    case "${o}" in
+        u)
+            protocol=${OPTARG}
+            ;;
+        p)
+            prefix=${OPTARG}
+            ;;
+        b)
+            b=${OPTARG}
+            ;;
+        s)
+            suffix=${OPTARG}
+            ;;
+        d)
+            product=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${prefix}" ] || [ -z "${b}" ]; then
+    usage
+fi
+
+IFS=' ' read -r -a prefixes <<< "$prefix"
+IFS=' ' read -r -a suffixes <<< "$suffix"
+IFS=' ' read -r -a products <<< "$product"
+first_suffix="${suffixes[0]}"
+
+# index new datasets
+# prepare script will add new records to the database
+for i in "${!prefixes[@]}"
+do
+    if [ -z "${suffixes[$i]}"  ] && [ -z "${first_suffix}" ]
+    then
+        suffix_string=""
+    elif [ -z "${suffixes[$i]}" ]
+    then
+        suffix_string="${first_suffix}"
+    else
+        suffix_string="${suffixes[$i]}"
+    fi
+    if [ "${protocol}" == "s3" ]
+    then
+        s3-find "s3://${b}/${prefixes[$i]}" ${suffix_string:+"*$suffix_string"} | \
+        s3-yaml-to-tar | \
+        dc-index-from-tar
+    elif [ "${protocol}" == "gs" ]
+    then
+        gs-to-tar --bucket ${b} --prefix ${prefixes[$i]}
+        dc-index-from-tar --protocol "${protocol}" metadata.tar.gz
+    elif [ "${protocol}" == "http" ]
+    then
+        thredds-to-tar -c "${b}/${prefixes[$i]}" \
+        -t ".*ARD-METADATA.yaml" -s '.*NBAR.*' -s '.*SUPPLEMENTARY.*' \
+        -s '.*NBART.*' -s '.*/QA/.*' -w 8
+        dc-index-from-tar --protocol "${protocol}" metadata.tar.gz
+    fi
+done
+
+# update ranges in wms database
+
+if [ -z "$product" ]
+then
+    python3 /code/update_ranges.py --calculate-extent
+else
+
+    for i in "${!products[@]}"
+    do
+        python3 /code/update_ranges.py --calculate-extent --product "${products[$i]}"
+    done
+fi
+
+#python3 /code/update_ranges.py --no-calculate-extent ${product:+"--product"} ${product:+"$product"}
---
+++ dockerfile
@@ -0,0 +1,18 @@
+FROM opendatacube/wms
+
+# Ensure compatible versions of boto are installed
+RUN pip3 uninstall boto3 botocore dea-proto -y && \
+    pip3 install -U 'aiobotocore[awscli,boto3]' google-cloud-storage
+
+# Install dea proto for indexing tools
+RUN pip3 install -e 'git+https://github.com/opendatacube/dea-proto.git#egg=project[GCP,THREDDS]'
+
+# Install C version of PyYaml
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    libyaml-dev \
+    && rm -rf /var/lib/apt/lists/* && \
+    pip3 --no-cache-dir install --verbose --force-reinstall -I pyyaml
+
+COPY assets/update_ranges.sh /code/index/indexing/update_ranges.sh
+COPY assets/update_ranges_wrapper.sh /code/index/indexing/update_ranges_wrapper.sh
+COPY assets/create-index.sh /code/index/create-index.sh
