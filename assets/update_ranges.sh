f7c2c5072032181000e4e7f0ea5ced1df5632860
tom-butler <tombutler.development@gmail.conm>
2019-04-08 09:44:37 +1000

    add multi-product and exclude support

--- assets/update_ranges.sh
+++ assets/update_ranges.sh
@@ -14,16 +14,16 @@
 
 usage() { echo "Usage: $0 -u <protocol> -p <prefix> -b <bucket> [-s <suffix>] [-i <ignore>] [-y UNSAFE]" 1>&2; exit 1; }
 
-while getopts ":u:p:b:s:i:y:d:" o; do
+while getopts ":u:p:b:s:i:y:d:l:e:" o; do
     case "${o}" in
         u)
             protocol=${OPTARG}
             ;;
-        p)
-            prefix=${OPTARG}
-            ;;
         b)
             b=${OPTARG}
+            ;;
+        p)
+            prefix=${OPTARG}
             ;;
         s)
             suffix=${OPTARG}
@@ -37,8 +37,14 @@ while getopts ":u:p:b:s:i:y:d:" o; do
         d)
             product=${OPTARG}
             ;;
+        m) 
+            multiproduct=${OPTARG}
+            ;;
         l)
-            lineage=${OPTARG}
+            ignore-lineage=${OPTARG}
+            ;;
+        e)
+            exclude=${OPTARG}
             ;;
     esac
 done
@@ -54,83 +60,57 @@ IFS=' ' read -r -a products <<< "$product"
 first_suffix="${suffixes[0]}"
 safety_arg=""
 
-if [ "$safety" == "SAFE" ]
-then
-    safety_arg="--skip-check"
-fi
-
 
 
 # index new datasets
-# prepare script will add new records to the database
-for i in "${!prefixes[@]}"
-do
-    # Set suffix string
-    if [ -z "${suffixes[$i]}"  ] && [ -z "${first_suffix}" ]
-    then
-        suffix_string=""
-    elif [ -z "${suffixes[$i]}" ]
-    then
-        suffix_string="${first_suffix}"
-    else
-        suffix_string="${suffixes[$i]}"
-    fi
+for i in "${!prefixes[@]}"; do
 
-    if [ -z "$lineage" ]
-    then
-        # Index 
-        if [ "${protocol}" == "s3" ]
-        then
-            s3-find $safety_arg "s3://${b}/${prefixes[$i]}" | \
-            s3-to-tar | \
-            dc-index-from-tar
-        elif [ "${protocol}" == "gs" ]
-        then
-            gs-to-tar --bucket ${b} --prefix ${prefixes[$i]}
-            dc-index-from-tar --protocol "${protocol}" metadata.tar.gz
-        elif [ "${protocol}" == "http" ]
-        then
-            # renders list as " -s item -s item ..." using $@
-            set -- $ignore
-            set -- "${@/#/ -s }"
+    # AWS S3 Bucket
+    if [ "${protocol}" == "s3" ]; then
 
-            thredds-to-tar -c "${b}/${prefixes[$i]}" -t $suffix_string -w 8 $@ 
-            dc-index-from-tar --protocol "${protocol}" metadata.tar.gz
+        if [ "$safety" == "SAFE" ]; then
+            safety_arg="--skip-check"
         fi
-    else
-        # Index without lineage
-        if [ "${protocol}" == "s3" ]
-        then
-            s3-find $safety_arg "s3://${b}/${prefixes[$i]}" | \
-            s3-to-tar | \
-            dc-index-from-tar --ignore-lineage
-        elif [ "${protocol}" == "gs" ]
-        then
-            gs-to-tar --bucket ${b} --prefix ${prefixes[$i]}
-            dc-index-from-tar --protocol "${protocol}" metadata.tar.gz --ignore-lineage
-        elif [ "${protocol}" == "http" ]
-        then
-            # renders list as " -s item -s item ..." using $@
-            set -- $ignore
-            set -- "${@/#/ -s }"
 
-            thredds-to-tar -c "${b}/${prefixes[$i]}" -t $suffix_string -w 8 $@ 
-            dc-index-from-tar --protocol "${protocol}" metadata.tar.gz --ignore-lineage
+        s3-find $safety_arg "s3://${b}/${prefixes[$i]}" | \
+        s3-to-tar | \
+        dc-index-from-tar ${exclude:+"--exclude"} ${exclude:+"$exclude"} ${ignore-lineage:+"--ignore-lineage"}
+
+    # Google Storage Bucket
+    elif [ "${protocol}" == "gs" ]; then
+        gs-to-tar --bucket ${b} --prefix ${prefixes[$i]}
+        dc-index-from-tar --protocol "${protocol}" metadata.tar.gz ${exclude:+"--exclude"} ${exclude:+"$exclude"} ${ignore-lineage:+"--ignore-lineage"}
+    
+    # NCI thredds server
+    elif [ "${protocol}" == "http" ]; then
+        # Set suffix string
+        if [ -z "${suffixes[$i]}"  ] && [ -z "${first_suffix}" ]; then
+            suffix_string=""
+        elif [ -z "${suffixes[$i]}" ]; then
+            suffix_string="${first_suffix}"
+        else
+            suffix_string="${suffixes[$i]}"
         fi
-    fi
+
+        # renders list as " -s item -s item ..." using $@
+        set -- $ignore
+        set -- "${@/#/ -s }"
+
+        thredds-to-tar -c "${b}/${prefixes[$i]}" -t $suffix_string -w 8 $@ 
+        dc-index-from-tar --protocol "${protocol}" metadata.tar.gz ${exclude:+"--exclude"} ${exclude:+"$exclude"} ${ignore-lineage:+"--ignore-lineage"}
 done
 
 # update ranges in wms database
-
-if [ -z "$product" ]
-then
+if [ -z "$product" ]; then
     python3 /code/update_ranges.py --no-calculate-extent
 else
-
-    for i in "${!products[@]}"
-    do
+    for i in "${!products[@]}"; do
         python3 /code/update_ranges.py --no-calculate-extent --product "${products[$i]}"
     done
 fi
-
-#python3 /code/update_ranges.py --no-calculate-extent ${product:+"--product"} ${product:+"$product"}
+# if multiproducts are set, update them too
+if [ -n "$multiproduct" ]; then
+    for i in "${!multiproducts[@]}"; do
+        python3 /code/update_ranges.py --no-calculate-extent --multiproduct "${multiproducts[$i]}"
+    done
+fi
