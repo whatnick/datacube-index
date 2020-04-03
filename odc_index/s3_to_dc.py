18578200393eb8ef212511fd947522e895d91493
Tisham Dhar <whatnickd@gmail.com>
2020-04-03 15:56:07 +1100

    Build a basic Minio + Postgres test harness for indexing (#18)
    * #17 Get started with docker-compose
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #17 working Minio/Postgres services
    
    * #17 Install odc-tools from fork, run command
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #17 Make black happy, need the pre-commit hook
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    Co-authored-by: whatnick <tisham@whatnick.com>

--- odc_index/s3_to_dc.py
+++ odc_index/s3_to_dc.py
@@ -2,6 +2,8 @@
 """Build S3 iterators using odc-tools
 and index datasets found into RDS
 """
+import sys
+
 import click
 from odc.aio import S3Fetcher
 from odc.aws._find import parse_query, norm_predicate
@@ -12,7 +14,14 @@ from odc.aws._find import parse_query, norm_predicate
 @click.argument("product", type=str, nargs=1)
 def cli(uri, product):
     """ Iterate through files in an S3 bucket and add them to datacube"""
-    pass
+    # Get a generator from supplied S3 Uri for metadata definitions
+    try:
+        qq = parse_query(uri)
+    except ValueError as e:
+        click.echo(str(e), err=True)
+        sys.exit(1)
+
+    # Consume generator to add YAML's to Datacube
 
 
 if __name__ == "__main__":
