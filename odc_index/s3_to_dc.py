f867b32cc999f2785d6270b3fd7751ac7620c6ab
Tisham Dhar <whatnickd@gmail.com>
2020-04-15 15:41:33 +1000

    Dump from S3 to ODC DB (#23)
    * #10 Call more odc-tools code
    
    * #10 Pass correctly to datacube index
    
    * #20 Make black happy
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #10 No terminal on CI
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #10 Add final step to send dataset to DB
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #10 Address PR comments
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #10 Make black happy
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #10 Use postgresql container
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    * #10 Don't allocate TTY
    
    Signed-off-by: whatnick <tisham@whatnick.com>
    
    Co-authored-by: whatnick <tisham@whatnick.com>

--- odc_index/s3_to_dc.py
+++ odc_index/s3_to_dc.py
@@ -3,9 +3,41 @@
 and index datasets found into RDS
 """
 import sys
+import logging
+from typing import Tuple
 
 import click
-from odc.aio import s3_find_glob
+from odc.aio import s3_find_glob, S3Fetcher
+from odc.index import from_yaml_doc_stream
+from datacube import Datacube
+
+
+def dump_to_odc(data_stream, dc: Datacube) -> Tuple[int, int]:
+    # TODO: Get right combination of flags for **kwargs in low validation/no-lineage mode
+    expand_stream = ((d.url, d.data) for d in data_stream if d.data is not None)
+
+    # TODO: Apply the eo3 transform
+    ds_stream = from_yaml_doc_stream(expand_stream, dc.index, transform=None)
+    ds_added = 0
+    ds_failed = 0
+    # Consume chained streams to DB
+    for result in ds_stream:
+        ds, err = result
+        if err is not None:
+            logging.error(err)
+            ds_failed += 1
+        else:
+            logging.info(ds)
+            # TODO: Potentially wrap this in transactions and batch to DB
+            # TODO: Capture UUID's from YAML and perform a bulk has
+            try:
+                dc.index.datasets.add(ds)
+                ds_added += 1
+            except Exception as e:
+                logging.error(e)
+                ds_failed += 1
+
+    return ds_added, ds_failed
 
 
 @click.command("s3-to-dc")
@@ -13,12 +45,23 @@ from odc.aio import s3_find_glob
 @click.argument("product", type=str, nargs=1)
 def cli(uri, product):
     """ Iterate through files in an S3 bucket and add them to datacube"""
-    # Get a generator from supplied S3 Uri for metadata definitions
-    s3_yaml_stream = s3_find_glob(uri, False)
+    # TODO: Have eo3 argument OR autodetect
 
-    # Consume generator to add YAML's to Datacube
-    for yaml in s3_yaml_stream:
-        print(yaml.url)
+    # Get a generator from supplied S3 Uri for metadata definitions
+    fetcher = S3Fetcher()
+
+    # TODO: Share Fetcher
+    s3_obj_stream = s3_find_glob(uri, False)
+
+    # Extract URL's from output of iterator before passing to Fetcher
+    s3_url_stream = (o.url for o in s3_obj_stream)
+
+    # TODO: Capture S3 URL's in batches and perform bulk_location_has
+
+    # Consume generator and fetch YAML's
+    dc = Datacube()
+    added, failed = dump_to_odc(fetcher(s3_url_stream), dc)
+    print(f"Added {added} Datasets, Failed {failed} Datasets")
 
 
 if __name__ == "__main__":
