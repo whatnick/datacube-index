a72024881e0ae72d66ba5f2a8c9dc74c94961e88
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-05-28 01:51:37 +0000

    #46 checkin shellchecked version of the script

---
+++ assets/bootstrap-odc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+############################################################
+# This script allows creation of all metadata/products     #
+# in an ODC instance from a given CSV catalog definition   #
+# In local development activate <odc> conda environment    #
+# In kubernetes pod executor run from <datacube-index>     #
+# container                                                #
+############################################################
+product_catalog=$1
+metadata_catalog=$2
+
+datacube system init --no-default-types --no-init-users
+# Created using : datacube metadata list | awk '{print $1}' | xargs datacube metadata show
+datacube metadata add "$metadata_catalog"
+wget "$product_catalog" -O product_list.csv
+tail -n+2 product_list.csv | awk -F, '{print $2}' | xargs datacube -v product add
